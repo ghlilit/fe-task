d87e7ec933fe87e86107f9b3f4c43cb6084d2c45
lilit-ghandilyan <lilit_ghandilyan@edu.aua.am>
2019-06-29 03:12:46 +0400

    add features and footer sections

---
+++ animation.js
@@ -0,0 +1,27 @@
+let keepFading = (start, prevWidth) => {
+    let currentWidth = $( window ).width();
+    let interval = currentWidth > 1000 ? 5 : 2;
+    prevWidth !== currentWidth ? start = 0 : '';
+    
+    let elements = $('.our-clients img').slice(start, start + interval);
+    elements.fadeIn(1500);
+    elements.fadeOut(1500).promise()
+    .then(() => { keepFading((start + interval) % 10, currentWidth);
+    });
+  }
+
+  keepFading(0, $( window ).width());
+
+$( window ).scroll(() => {
+  $(".feature").each( function(){
+            
+    let bottom_of_object = $(this).position().top + $(this).outerHeight();
+    let bottom_of_window = $(window).scrollTop() + $(window).height();
+    
+    if( bottom_of_window > bottom_of_object ){
+        $(this).animate({'opacity':'1'}, 1000);
+    }
+
+  }); 
+})
+
