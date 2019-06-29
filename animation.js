7cc38d42596b7288d494511e941227a6c185bdd4
lilit-ghandilyan <lilit_ghandilyan@edu.aua.am>
2019-06-29 20:23:25 +0400

    add media queries

--- animation.js
+++ animation.js
@@ -1,6 +1,6 @@
 let keepFading = (start, prevWidth) => {
     let currentWidth = $( window ).width();
-    let interval = currentWidth > 1000 ? 5 : 2;
+    let interval = currentWidth > 768 ? 5 : 2;
     prevWidth !== currentWidth ? start = 0 : '';
     
     let elements = $('.our-clients img').slice(start, start + interval);
@@ -13,13 +13,13 @@ let keepFading = (start, prevWidth) => {
   keepFading(0, $( window ).width());
 
 $( window ).scroll(() => {
-  $(".feature").each( function(){
+  $(".hideme").each( function(){
             
     let bottom_of_object = $(this).position().top + $(this).outerHeight();
     let bottom_of_window = $(window).scrollTop() + $(window).height();
     
     if( bottom_of_window > bottom_of_object ){
-        $(this).animate({'opacity':'1'}, 1000);
+        $(this).animate({'opacity':'1'}, 1500);
     }
 
   }); 
